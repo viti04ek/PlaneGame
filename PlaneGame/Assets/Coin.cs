cbd1d8f7a1de318c16870d81389b3839668e3fee
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-03-10 16:01:44 +0200

    Coin animation
    ADDED:
    - Coin rotation animation

--- PlaneGame/Assets/Coin.cs
+++ PlaneGame/Assets/Coin.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public void Update()
+    {
+        transform.Rotate(0, 150 * Time.deltaTime, 0);
+    }
+
+
     public void Die()
     {
         Destroy(gameObject);
